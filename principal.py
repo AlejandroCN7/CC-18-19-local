a758285dfb726f4a80b5ed745ae6e2baa43dc6a1
AlejandroCN7 <alejandroac79@gmail.com>
2018-12-03 19:55:01 +0100

    Actualizada la implementación del servicio web con flask, ahora se tiene en cuenta la base de datos para proporcionar el servicio. Añadida petición REST DELETE a la URI /jugadores. Cambiada forma de definir las rutas de los jugadores individuales (ahora es por nick) close #27

--- principal.py
+++ principal.py
@@ -1,41 +1,54 @@
 from flask import Flask
 from flask_restful import Resource, Api, abort, reqparse
 from model import Jugador
+from mongoDB import BaseDatos
+import pymongo
+import json
 
 import os
 
-app = Flask("hito2")
+
+app = Flask("hito3")
 api = Api(app)
 
-
-# Esto sería con flask sin el microframework de RestFul
+# Esto ser?a con flask sin el microframework de RestFul
 #@app.route("/")
 
 #def hello():
     #return "Hola Mundo !! :)
 
-j1 = Jugador("Hapneck","Alejandro","Campoy Nieves",22,["Fortnite","Hollow Knight","The Witcher"],None)
+j1 = Jugador("Hapneck","Alejandro","Campoy Nieves",22,["Fortnite","Hollow Knight","The Witcher"],True)
 j2 = Jugador("Malcaide","Alfonso","Barragan Lara",22,["Counter Strike"],True)
 j3 = Jugador("Rekkles","Juan","Martinez Casado",22,["Fortnite","League of Legends","Counter Strike"],False)
 
 
-recursos = {"jugador1":j1.__dict__(),
-            "jugador2":j2.__dict__(),
-            "jugador3":j3.__dict__()
-            }
+#recursos = {"jugador1":j1.__dict__(),
+#            "jugador2":j2.__dict__(),
+#            "jugador3":j3.__dict__()
+#            }
+
+mongo = BaseDatos("mongodb://Alejandro:alejandro13@ds026018.mlab.com:26018/jugadores")
+
+mongo.insertJugador(j1)
+mongo.insertJugador(j2)
+mongo.insertJugador(j3)
+
+
 
 parser = reqparse.RequestParser()
-parser.add_argument('Nick', type=str, help='El nick debe ser único',required = True)
+parser.add_argument('Nick', type=str, help='El nick debe ser ?nico',required = True)
 parser.add_argument('Nombre', type=str, required = True)
 parser.add_argument('Apellidos', type=str, required = True)
 parser.add_argument('Edad', type=int, required = True)
 parser.add_argument('Videojuegos', required = True, action="append")
-parser.add_argument('Competitivo', type=bool, required = True)
+parser.add_argument('Competitivo', type=str, required = True)
 
 
 def abortar_ruta_inexistente(ruta):
-    if ruta not in recursos:
-        abort(404, message="Error 404. La ruta {} no existe".format(ruta))
+    for j in mongo.jugadores.find():
+        if(ruta == j['Nick']):
+            return
+    abort(404, message="Error 404. La ruta {} no existe".format(ruta))
 
 class Principal(Resource):
 
@@ -46,32 +59,37 @@ class JugadorIndividual(Resource):
 
     def get(self,ruta):
         abortar_ruta_inexistente(ruta)
-        return {ruta:recursos[ruta]}
+        return {ruta:mongo.getJugador(ruta)}
 
     def put(self,ruta):
         args = parser.parse_args()
         jugador = Jugador(args['Nick'], args['Nombre'], args['Apellidos'], args['Edad'],
                           args['Videojuegos'], args['Competitivo'])
-        recursos[ruta]=jugador.__dict__()
-        return recursos[ruta]
+        exito = mongo.insertJugador(jugador)
+        if(not(exito)):
+            mongo.updateJugador(args['Nick'],jugador.__dict__())
+        return mongo.getJugador(ruta)
 
     def delete(self,ruta):
-        if(ruta in recursos.keys()):
-            del recursos[ruta]
+        mongo.removeJugador(ruta)
         return '',204
 
 class Jugadores(Resource):
 
     def get(self):
-        return recursos
+        return mongo.getJugadores()
 
     def post(self):
         args = parser.parse_args()
-        ruta = "jugador" + str(len(recursos)+1)
         jugador = Jugador(args['Nick'], args['Nombre'], args['Apellidos'], args['Edad'],
                           args['Videojuegos'], args['Competitivo'])
-        recursos[ruta]=jugador.__dict__()
-        return recursos[ruta],201
+        ruta = args['Nick']
+        mongo.insertJugador(jugador)
+        return mongo.getJugador(ruta),201
+
+    def delete(self):
+        mongo.removeJugadores()
+        return '',204
 
 api.add_resource(Principal,'/','/principal')
 api.add_resource(Jugadores,'/jugadores')
